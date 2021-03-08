98ca084e08a299253d347ed3e67cbb0195739a60
BigWizards <95jsjones@gmail.com>
2021-03-08 18:09:06 +0000

    Add user_id column to post table

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_170339) do
+ActiveRecord::Schema.define(version: 2021_03_08_172934) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "link"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -25,4 +26,5 @@ ActiveRecord::Schema.define(version: 2021_03_08_170339) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "posts", "users"
 end
