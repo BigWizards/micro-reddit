d8b4e5f9884014c3565e86ed77826a3a7b6f4982
BigWizards <95jsjones@gmail.com>
2021-03-09 17:59:40 +0000

    Update associations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   validates :body, presence: true, length: {minimum: 3, maximum:300}
   
-  belongs_to :user, class_name: "user", foreign_key: "user_id"
-  belongs_to :post, class_name: "post", foreign_key: "post_id"
+  belongs_to :user
+  belongs_to :post
 end
